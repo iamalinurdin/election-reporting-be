1d1988be871df6d3cb742f2d8139c666d3264700
Ali Nurdin <alinurdin.dev@gmail.com>
2024-09-05 20:04:06 +0700

    add migrations

---
+++ database/migrations/2024_09_05_125516_add_field_election_voters_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::table('election_voters', function (Blueprint $table) {
+      $table->enum('voter_type', ['dpt', 'non-dpt'])->nullable()->after('evidence');
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::table('election_voters', function (Blueprint $table) {
+      $table->dropColumn('voter_type');
+    });
+  }
+};
